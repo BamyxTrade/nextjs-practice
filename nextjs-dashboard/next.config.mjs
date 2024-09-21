b8d77e09ede43bef8bb0711504f378fd02c7ee10
Bamyx <bamyxtrade@gmail.com>
2024-09-21 18:11:52 +0800

    first commit

---
+++ nextjs-dashboard/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    // basePath: "/nextjs-dashboard",
+    // output: "export",  // <=== enables static exports
+    // reactStrictMode: true,
+};
+
+export default nextConfig;
