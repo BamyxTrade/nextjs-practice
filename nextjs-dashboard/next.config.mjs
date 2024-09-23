1a47f4d24119119775077fba82bb78ed63c5b644
Bamyx <bamyxtrade@gmail.com>
2024-09-23 11:27:58 +0800

    Chapter 10

--- nextjs-dashboard/next.config.mjs
+++ nextjs-dashboard/next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     // basePath: "/nextjs-dashboard",
     // output: "export",  // <=== enables static exports
     // reactStrictMode: true,
+    experimental: {
+     ppr: 'incremental',
+    },
 };
 
 export default nextConfig;
